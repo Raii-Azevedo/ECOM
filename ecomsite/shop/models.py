4477b5a5d28e9cfd12b131b7d49cf80918b01362
Raissa Azevedo <rhaii.azevedo@gmail.com>
2024-01-10 22:07:25 -0300

    Models - Product

--- ecomsite/shop/models.py
+++ ecomsite/shop/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    product = models.CharField(max_length = 200)
+    price = models.FloatField()
+    discount_price = models.FloatField()
+    category = models.CharField(max_length = 200)
+    description = models.TextField()
+    image = models.CharField(max_length=300)
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return self.product
