9dbe888048f9c81865ca70d378943a275496a08d
Raissa Azevedo <rhaii.azevedo@gmail.com>
2024-01-13 12:44:16 -0300

    Search functionality

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -5,5 +5,9 @@ from .models import Product
 
 def index(request):
     product_objects = Product.objects.all()
+
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        product_objects = product_objects.filter(product__icontains=item_name)
     return render(request, 'shop/index.html', {'product_objects': product_objects})
 
