0ceca43a465d8668b5e5d0350ffc4dfef9f3752d
Raissa Azevedo <rhaii.azevedo@gmail.com>
2024-01-13 11:57:12 -0300

    CSS for Shop

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def index(request):
+    product_objects = Product.objects.all()
+    return render(request, 'shop/index.html', {'product_objects': product_objects})
+
