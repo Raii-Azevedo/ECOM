3a8bb698846ab8fcd83f126900831244a4ee2aa2
Raissa Azevedo <rhaii.azevedo@gmail.com>
2024-02-17 15:29:42 -0300

    Popover problem

--- ecomsite/shop/static/shop/script.js
+++ ecomsite/shop/static/shop/script.js
@@ -1,11 +1,16 @@
+// Popover
+$(function () {
+    $('[data-toggle="popover"]').popover();
+    document.getElementById("cart").setAttribute('data-content', '<h5>This is your cart</h5>')
+});
+
 // Creating The Cart Logic
-if(localStorage.getItem('cart')==null){
+if(localStorage.getItem('cart') == null){
     var cart = {}; // {} -> Indicates a JSON object
 }
 else {
     cart = JSON.parse(localStorage.getItem('cart')); // To make sure the items in the 'cart' are the same
 }
-
 
 // Use CLASS to access the Add Cart Button
 $(document).on('click', '.atc', function(){
@@ -15,8 +20,8 @@ $(document).on('click', '.atc', function(){
     var item_id = this.id.toString();
     console.log(item_id);
 
-    // Checking it the item is in the cart --> Saving Items in the cart
-    if(cart[item_id]!=undefined){
+    // Checking if the item is in the cart --> Saving Items in the cart
+    if(cart[item_id] != undefined){
         cart[item_id] = cart[item_id] + 1;
     }
     else{
@@ -25,6 +30,5 @@ $(document).on('click', '.atc', function(){
     console.log(cart);
     // Saving items in the cart
     localStorage.setItem('cart',JSON.stringify(cart));
+    document.getElementById("cart").innerHTML = "Cart("+ Object.keys(cart).length + ")";
 });
-
-
